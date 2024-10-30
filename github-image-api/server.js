549ba9df2d744f13552e813c35924611c50f64a5
agoshrao <agoshrao@yahoo.com>
2024-10-30 10:51:19 +0530

    Update server.js

--- github-image-api/server.js
+++ github-image-api/server.js
@@ -22,10 +22,13 @@ async function fetchImageList(folder) {
       headers: { 'Accept': 'application/vnd.github.v3+json' },
     });
 
-    // Filter for images (you may want to adjust the regex if there are other files)
-    return response.data
+    // Filter for images
+    const imageFiles = response.data
       .filter(file => file.type === 'file' && /\.(jpg|jpeg|png)$/i.test(file.name))
       .map(file => file.name);
+    
+    console.log(`Fetched images from ${folder}:`, imageFiles);
+    return imageFiles;
   } catch (error) {
     console.error(`Failed to fetch image list from ${folder}:`, error.message);
     return [];
@@ -36,31 +39,26 @@ async function fetchImageList(folder) {
 app.get('/random-image', async (req, res) => {
   const gender = req.query.gender;
 
-  // Determine folder based on gender parameter
   let folder;
   if (gender === 'male') {
     folder = 'gentlemen';
   } else if (gender === 'female') {
     folder = 'ladies';
   } else {
-    // If gender is 'random' or unspecified, choose randomly between gentlemen and ladies
-    const folderNames = ['gentlemen', 'ladies'];
-    folder = folderNames[Math.floor(Math.random() * folderNames.length)];
+    folder = Math.random() < 0.5 ? 'gentlemen' : 'ladies';
   }
 
   try {
-    // Get the list of images dynamically
     const images = await fetchImageList(folder);
 
     if (images.length === 0) {
       return res.status(404).send(`No images found in folder: ${folder}`);
     }
 
-    // Select a random image from the list
     const randomImage = images[Math.floor(Math.random() * images.length)];
     const imageUrl = `https://raw.githubusercontent.com/${GITHUB_USERNAME}/${GITHUB_REPOSITORY}/${GITHUB_BRANCH}/${GITHUB_BASE_FOLDER}/${folder}/${randomImage}`;
 
-    // Fetch the image from GitHub and return it
+    console.log(`Fetching image: ${imageUrl}`);
     const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
     res.setHeader('Content-Type', 'image/jpeg');
     res.send(response.data);
